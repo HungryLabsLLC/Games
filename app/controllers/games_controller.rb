0d7711252634f285836a5ebdb3d31923fd3f5448
Erol Erturk <erol@ubicare.com>
2014-02-12 10:53:44 -0500

    tic tac toe

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+
+  def tic_tac_toe
+  end
+
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicTacToe::Application.config.secret_key_base = '31bb9d4b0e94a4868b4b5779ae4c4accae06b6e43d73a7a4ff68f0074b1c89723322a975f8c0674f7b2f84e6a0a6ae1ef52de66466b80f8724f558712615e931'
